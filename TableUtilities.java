2cd42d983fd62a9677de8b4bef95736cb64b967f
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-02-06 17:58:44 -0500

    completed all exercises

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,32 @@
- 
+
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+      return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String str = "";
+        for(int i = 1; i <= tableSize; i++){
+          for(int j = 1; j <= tableSize; j++){
+              int number = i * j;
+              if(number < 10){
+                str+= "  " + number + " |";
+              }
+              else if(number < 100){
+                str+= " " + number + " |";
+              }
+              else{
+                str+= number + " |";
+              }
+
+          }
+          str += "\n";
+        }
+        return str;
     }
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -1,21 +1,32 @@
- 
+
 
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String str = "";
+        for(int i = 0; i < numberOfStars; i++){
+          str += "*";
+        }
+        return str;
     }
-    
+
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String str = "";
+        for(int i = 1; i <= numberOfRows; i++){
+          for(int j = 1; j <= i; j++){
+            str += "*";
+          }
+          str += "\n";
+        }
+        return str;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(9);
     }
 }
